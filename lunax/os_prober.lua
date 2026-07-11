6850a023fdae27cc42f872f688dcdf1605a29474
Jiafei <cxkctrl1303@hotmail.com>
2026-07-11 17:26:26 +0800

    refactor(os_prober): improve perf

--- lunax/os_prober.lua
+++ lunax/os_prober.lua
@@ -1,14 +1,12 @@
-return (function()
-    do  -- Windows
-        local handle <close> = assert(io.popen('cd'))
-        local res = handle:read('*l')
-        if res and res:match('^[A-Z]:\\') then
-            return 'NT'
-        end
+local OS = (function()
+    -- Windows
+    if string.sub(package.config, 1, 1) == '\\' then
+        return "NT"
     end
 
-    do  -- Unix
-        local handle <close> = assert(io.popen('uname -s'))
-        return handle:read('*l')
-    end
+    -- Unix
+    local handle <close> = assert(io.popen('uname -s'))
+    return handle:read('*l')
 end)()
+
+return OS
