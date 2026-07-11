167c9da67b0fc768427927ec1200a4a1048442d3
Jiafei <cxkctrl1303@hotmail.com>
2026-07-11 17:49:59 +0800

    feat: new modules
    - `exec` & `popen`

--- lunax/exec.lua
+++ lunax/exec.lua
@@ -1,16 +1,17 @@
-local function fmt_err(idx, type, suffix)
-    local err = ('bad arg#%d for exec(): expected %s'):format(idx, type)
-    err = suffix and err..' '..suffix or err
-
-    error(err)
-end
+local util = require('lunax.util')
+local unix = require('lunax.os_prober') ~= 'NT'
+local fmt = util.fmt_type_err
 
 local function exec(cmd, conf)
     -- 命令
     if type(cmd) == 'table' then
+        if not util.is_array(cmd) then
+            error(fmt(1, 'exec', 'array or string', 'map'))
+        end
+
         cmd = table.concat(cmd, ' ')
     elseif type(cmd) ~= 'string' then
-        fmt_err(1, 'table or string')
+        error(fmt(1, 'exec', 'array or string', type(cmd)))
     end
 
     cmd = '('..cmd..')'
@@ -18,24 +19,22 @@ local function exec(cmd, conf)
     -- 当前目录
     if type(conf.cwd) == 'string' then
         cmd = (' cd %q; '):format(conf.cwd)..cmd
-    elseif conf.cmd ~= nil then
-        fmt_err(2, 'string or nil', 'at cwd')
+    elseif conf.cwd ~= nil then
+        error(fmt(2, 'exec(_, conf.cwd)', 'string', type(conf.cwd)))
     end
-
-    local unix = require('lunax.os_prober') ~= 'NT'
 
     -- 环境变量
     if type(conf.env) == 'table' then
         for k,v in pairs(conf.env) do
             if type(k) ~= 'string' or type(v) ~= 'string' then
-                fmt_err(2, 'string', 'at env.k&v')
+                error(fmt(2, 'exec(_, conf.env)', 'map<string, string>', 'map<T, T>'))
             end
 
             cmd = unix and (' export %s="%s"; '):format(k, v)..cmd
                 or (' set %s=%s; '):format(k, v)..cmd
         end
     elseif conf.env ~= nil then
-        fmt_err(2, 'table or nil', 'at env')
+        error(fmt(2, 'exec(_, conf.env)', 'map<string, string>', type(conf.env)))
     end
 
     -- 标准/异常输出
@@ -51,11 +50,12 @@ local function exec(cmd, conf)
             cmd = unix and cmd..(' %d> /dev/null '):format(v)
                 or cmd..(' %d> NUL '):format(v)
         elseif conf[k] ~= nil then
-            fmt_err(2, 'string or boolean or nil', 'at '..k)
+            fmt(2, ('exec(_, conf.%s)'):format(k), 'string or boolean or nil', type(conf[k]))
         end
     end
 
     -- 执行命令
+    cmd = not unix and 'chcp 65001; '..cmd or cmd
     return os.execute(cmd)
 end
 
--- lunax/popen.lua
+++ lunax/popen.lua
@@ -1,16 +1,16 @@
-local function fmt_err(idx, type, suffix)
-    local err = ('bad arg#%d for exec(): expected %s'):format(idx, type)
-    err = suffix and err..' '..suffix or err
-
-    error(err)
-end
+local util = require('lunax.util')
+local fmt = util.fmt_type_err
 
 local function popen(cmd, conf, mode)
     -- 命令
     if type(cmd) == 'table' then
+        if not util.is_array(cmd) then
+            error(fmt(1, 'exec', 'array or string', 'map'))
+        end
+
         cmd = table.concat(cmd, ' ')
     elseif type(cmd) ~= 'string' then
-        fmt_err(1, 'table or string')
+        error(fmt(1, 'exec', 'array or string', type(cmd)))
     end
 
     cmd = '('..cmd..')'
@@ -19,7 +19,7 @@ local function popen(cmd, conf, mode)
     if type(conf.cwd) == 'string' then
         cmd = (' cd %q; '):format(conf.cwd)..conf.cwd
     elseif conf.cmd ~= nil then
-        fmt_err(2, 'string or nil', 'at cwd')
+        error(fmt(2, 'exec(_, conf.cwd)', 'string', type(conf.cwd)))
     end
 
     local unix = require('lunax.os_prober') ~= 'NT'
@@ -28,14 +28,14 @@ local function popen(cmd, conf, mode)
     if type(conf.env) == 'table' then
         for k,v in pairs(conf.env) do
             if type(k) ~= 'string' or type(v) ~= 'string' then
-                fmt_err(2, 'string', 'at env.k&v')
+                error(fmt(2, 'exec(_, conf.env)', 'map<string, string>', 'map<T, T>'))
             end
 
             cmd = unix and (' export %s="%s"; '):format(k, v)..cmd
                 or (' set %s=%s; '):format(k, v)..cmd
         end
     elseif conf.env ~= nil then
-        fmt_err(2, 'table or nil', 'at env')
+        error(fmt(2, 'exec(_, conf.env)', 'map<string, string>', type(conf.env)))
     end
 
     -- 标准/异常输出
@@ -58,10 +58,12 @@ local function popen(cmd, conf, mode)
                 cmd = cmd..' 2>&1 '
             end
         elseif conf[k] ~= nil then
-            fmt_err(2, 'string or boolean or nil', 'at '..k)
+            fmt(2, ('exec(_, conf.%s)'):format(k), 'string or boolean or nil', type(conf[k]))
         end
     end
 
+    cmd = not unix and 'chcp 65001; '..cmd or cmd
+    -- print(cmd)
     return io.popen(cmd, mode)
 end
 
