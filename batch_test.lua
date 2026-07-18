d20f221a58c03a7cb81984fc97e6243854183ae5
Jiafei <cxkctrl1303@hotmail.com>
2026-07-18 17:34:40 +0800

    fix(test): batch_test on unix

--- batch_test.lua
+++ batch_test.lua
@@ -1,7 +1,9 @@
 local fs = require('lunax.fs')
+local unix = require('lunax.os_prober') ~= 'NT'
 
 for _,file in ipairs(fs.ls('tests')) do
-    local ok, ext, code = os.execute(('"%q %q"'):format(arg[-1], fs.join('tests', file)))
+    local template = unix and '%s %q' or '"%q %q"'
+    local ok, ext, code = os.execute((template):format(arg[-1], fs.join('tests', file)))
     if not ok then
         io.stderr:write(('batch_test: { ext: %s, code: %d }\n'):format(ext, code))
         os.exit(1)
