7e6328b5632c42cc244f2da7cc1277dad8effbc2
Jiafei <cxkctrl1303@hotmail.com>
2026-08-02 12:55:50 +0800

    refactor(tty): return number instead of string

--- lunax/tty.lua
+++ lunax/tty.lua
@@ -4,22 +4,22 @@ local unix = require('lunax.os_prober') ~= 'NT'
 local tty = {}
 
 --- 获取终端尺寸
----@return string? rows 行数
----@return string? cols 列数
+---@return number? rows 行数
+---@return number? cols 列数
 function tty.term_size()
     if unix then
         local handle = io.popen('stty size < /dev/tty ')
         local rows, cols = handle:read('*l'):match("(%d+)%s+(%d+)")
         handle:close()
 
-        return rows, cols
+        return tonumber(rows), tonumber(cols)
     else
         local handle = io.popen('mode')
         local res = handle:read('*a')
         local rows = res:match('Lines:%s*(%d+)')
         local cols = res:match('Columns:%s*(%d+)')
 
-        return rows, cols
+        return tonumber(rows), tonumber(cols)
     end
 end
 
